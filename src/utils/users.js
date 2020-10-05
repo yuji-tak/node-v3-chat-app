d6c218c567874ab16fd809ab316a71c7dc93263f
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-05 11:51:43 +0900

    169

--- src/utils/users.js
+++ src/utils/users.js
@@ -17,7 +17,7 @@ const addUser = ({ id, username, room }) => {
 
   // Check for existing user
   const existingUser = users.find((user) => {
-    // usernameとroomが未登録であることを確認
+    // usernameとroomの2項目が未登録であることを確認
     return user.room === room && user.username === username
   })
 
@@ -43,17 +43,40 @@ const removeUser = (id) => {
   }
 }
 
-addUser({
-  id: 22,
-  username: 'Yuji',
-  room: 'Osaka'
-})
-addUser({
-  id: 22,
-  username: 'Yuji2',
-  room: 'Osaka'
-})
+const getUser = (id) => {
+  return users.find((user) => user.id === id)
+}
 
-removeUser(22)
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase()
+  return users.filter((user) => user.room === room)
+}
 
-console.log(users)
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
+
+// addUser({
+//   id: 1,
+//   username: 'Yuji1',
+//   room: 'Osaka'
+// })
+// addUser({
+//   id: 2,
+//   username: 'Yuji2',
+//   room: 'Osaka'
+// })
+// addUser({
+//   id: 3,
+//   username: 'Yuji3',
+//   room: 'Tokyo'
+// })
+
+// const user = getUser(1)
+// console.log(user)
+
+// const userList = getUsersInRoom('Osaka')
+// console.log(userList)
