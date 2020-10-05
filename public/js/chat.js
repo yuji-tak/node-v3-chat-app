982111146f05c7ac8a6ee0ad032b2a99d9e2a1e2
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-05 17:14:21 +0900

    173

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,28 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage)
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight
+
+  // Height of message container
+  const containerHeight = $messages.scrollHeight
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight
+  }
+}
+
 // ⭐️socket start!
 socket.on('message', (message) => {
   console.log(message)
@@ -25,6 +47,7 @@ socket.on('message', (message) => {
     createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoscroll()
 })
 
 socket.on('locationMessage', (message) => {
@@ -35,6 +58,7 @@ socket.on('locationMessage', (message) => {
     createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoscroll()
 })
 
 socket.on('roomData', ({ room, users }) => {
